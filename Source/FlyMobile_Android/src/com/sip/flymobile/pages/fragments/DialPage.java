2e7d362185eb7f425764562ac195d4a27d8141dd
JKH <jyyblue1987@yahoo.com>
2016-02-10 16:30:20 -0800

    [trying] change dial page.

--- Source/FlyMobile_Android/src/com/sip/flymobile/pages/fragments/DialPage.java
+++ Source/FlyMobile_Android/src/com/sip/flymobile/pages/fragments/DialPage.java
@@ -4,9 +4,12 @@ import com.sip.flymobile.R;
 import com.sip.flymobile.mvp.BasePageDecorator;
 import com.sip.flymobile.mvp.BaseView;
 
+import android.util.TypedValue;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import common.design.layout.LayoutUtils;
+import common.design.layout.ScreenAdapter;
 
 
 public class DialPage extends BasePageDecorator {
@@ -16,7 +19,7 @@ public class DialPage extends BasePageDecorator {
 	TextView	m_txtCredit = null;
 	TextView	m_txtChargeCredit = null;
 	
-	Button		m_btnPrefix = null;
+	Button		m_btnAddContact = null;
 	TextView 	m_txtPrefix = null;
 	TextView	m_txtDialNumber = null;
 	Button		m_btnDel = null;
@@ -39,6 +42,19 @@ public class DialPage extends BasePageDecorator {
 		m_txtMobile = (TextView) getContext().findViewById(R.id.txt_mobile);
 		m_txtExpireDate = (TextView) getContext().findViewById(R.id.txt_expire_date);
 		m_txtCredit = (TextView) getContext().findViewById(R.id.txt_credit_balance);
+		m_txtChargeCredit = (TextView) getContext().findViewById(R.id.txt_charge_credit);
+		
+		m_btnAddContact = (Button) getContext().findViewById(R.id.btn_add_contact);
+		m_txtPrefix = (TextView) getContext().findViewById(R.id.txt_prefix);
+		m_txtDialNumber = (TextView) getContext().findViewById(R.id.txt_dial_number);
+		m_btnDel = (Button) getContext().findViewById(R.id.btn_del);
+		
+		m_btnSwitchNumber = (Button) getContext().findViewById(R.id.btn_switch);
+		m_txtContactNumber = (TextView) getContext().findViewById(R.id.txt_contact_number);
+		
+		m_btnDial = (Button) getContext().findViewById(R.id.btn_dial);
+		m_btnNotification = (Button) getContext().findViewById(R.id.btn_noti_callback);
+		
 	}
 		
 	public void initData()
@@ -51,6 +67,38 @@ public class DialPage extends BasePageDecorator {
 	{
 		super.layoutControls();
 		
+		LayoutUtils.setMargin(getContext().findViewById(R.id.lay_total_self_info), 0, 40, 0, 40, true);
+		LayoutUtils.setSize(m_imgContactIcon, 200, 200, true);
+		LayoutUtils.setMargin(getContext().findViewById(R.id.lay_self_info), 80, 0, 0, 0, true);
+		
+		m_txtMobile.setTextSize(TypedValue.COMPLEX_UNIT_PX, ScreenAdapter.computeHeight(72));
+		
+		int fontsize = 35;
+		LayoutUtils.setMargin(getContext().findViewById(R.id.lay_expire), 0, 20, 0, 0, true);
+		((TextView)getContext().findViewById(R.id.lbl_expire_date)).setTextSize(TypedValue.COMPLEX_UNIT_PX, ScreenAdapter.computeHeight(fontsize));
+		m_txtExpireDate.setTextSize(TypedValue.COMPLEX_UNIT_PX, ScreenAdapter.computeHeight(fontsize));
+		
+		LayoutUtils.setMargin(getContext().findViewById(R.id.lay_credit), 0, 20, 0, 0, true);
+		((TextView)getContext().findViewById(R.id.lbl_credit_balance)).setTextSize(TypedValue.COMPLEX_UNIT_PX, ScreenAdapter.computeHeight(fontsize));
+		m_txtCredit.setTextSize(TypedValue.COMPLEX_UNIT_PX, ScreenAdapter.computeHeight(fontsize));
+		LayoutUtils.setMargin(m_txtChargeCredit, 50, 0, 0, 0, true);
+		m_txtChargeCredit.setTextSize(TypedValue.COMPLEX_UNIT_PX, ScreenAdapter.computeHeight(fontsize));
+		
+		LayoutUtils.setSize(m_btnAddContact, 62, 62, true);
+		LayoutUtils.setMargin(m_txtPrefix, 43, 0, 0, 0, true);
+		LayoutUtils.setMargin(m_txtDialNumber, 43, 0, 43, 0, true);
+		LayoutUtils.setSize(m_btnDel, 84, 60, true);
+		
+		((TextView)getContext().findViewById(R.id.lbl_display_number)).setTextSize(TypedValue.COMPLEX_UNIT_PX, ScreenAdapter.computeHeight(fontsize));
+		LayoutUtils.setSize(m_btnSwitchNumber, 50, 50, true);
+		LayoutUtils.setMargin(m_txtContactNumber, 30, 0, 0, 0, true);
+		m_txtContactNumber.setTextSize(TypedValue.COMPLEX_UNIT_PX, ScreenAdapter.computeHeight(50));
+		
+		
+		LayoutUtils.setSize(m_btnDial, 116, 116, true);
+		
+		LayoutUtils.setMargin(m_btnNotification, 190, 0, 0, 0, true);		
+		LayoutUtils.setSize(m_btnNotification, 116, 116, true);
 		
 	}
 }
